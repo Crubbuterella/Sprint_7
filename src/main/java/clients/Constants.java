f76397d1be6721e6c2d30d10355ed1518fafaca9
Ирина Шилова <nikatulka@mail.ru>
2023-06-05 00:04:24 +0300

    Sprint_7_V1.0

---
+++ src/main/java/clients/Constants.java
@@ -0,0 +1,13 @@
+package clients;
+import io.restassured.RestAssured;
+
+public class Constants {
+    public final static String CREATE_COURIER_ENDPOINT = "/api/v1/courier";
+    public final static String LOGIN_COURIER_ENDPOINT ="/api/v1/courier/login";
+    public final static String DELETE_COURIER_ENDPOINT ="/api/v1/courier/";
+    public final static String CREATE_ORDER_ENDPOINT = "/api/v1/orders";
+    public void setUp(){
+
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+}
